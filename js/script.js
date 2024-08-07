d1b24f1e70a8ecf7ffe57f62f059177321e14f30
danmlarsen <larsendanmarius@gmail.com>
2024-08-07 11:53:11 +0200

    Mostly finished

--- js/script.js
+++ js/script.js
@@ -7,18 +7,20 @@ const state = {
 };
 
 const fetchData = function () {
-    fetch('/data.json')
+    fetch('./data.json')
         .then(req => {
             if (!req.ok) {
-                console.error('Ooops. Something went wrong!');
-                return null;
+                throw new Error('Ooops. Something went wrong!');
             }
 
             return req.json();
         })
         .then(data => {
             state.data = data;
-            updateTimeTracker();
+            displayTimeTracker();
+        })
+        .catch(err => {
+            displayError(err);
         });
 };
 
@@ -27,13 +29,17 @@ const generateListItemMarkup = function () {
         const { timeframes, title } = item;
         const { current, previous } = timeframes[state.selectedTimeframe];
 
+        const previousText = state.selectedTimeframe == 'daily' ? 'Yesterday' : state.selectedTimeframe == 'weekly' ? 'Last week' : 'Last month';
+
         return `
             <li class="time-tracker__list-item time-tracker__list-item--${title.toLowerCase().replace(' ', '')}">
                 <div class="time-tracker__list-item-content">
                 <h2 class="time-tracker__category">${title}</h2>
-                <a href="#" class="time-tracker__options"><img src="images/icon-ellipsis.svg" class="time-tracker__options-icon" alt="" /></a>
+                <a href="#" class="time-tracker__options">
+                <svg class="time-tracker__options-icon" alt="Options button" width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill-rule="evenodd"/></svg>
+                </a>
                 <p class="time-tracker__hours">${current}hrs</p>
-                <p class="time-tracker__previous-hours">Previous - ${previous}hrs</p>
+                <p class="time-tracker__previous-hours">${previousText} - ${previous}hrs</p>
                 </div>
             </li>`;
     });
@@ -41,21 +47,31 @@ const generateListItemMarkup = function () {
     return listItemsMarkup.join('');
 };
 
-const updateTimeTracker = function () {
+const displayTimeTracker = function () {
     listContainer.innerHTML = `
     <ul class="time-tracker__list">${generateListItemMarkup()}</ul>
   `;
 };
 
+const displayError = function (errorMsg) {
+    listContainer.innerHTML = `
+        <div class="error">
+            <p class="error__message">${errorMsg}</p>
+        </div>
+    `;
+};
+
 timeframeListEl.addEventListener('click', function (e) {
-    if (!e.target.dataset?.timeframe) return;
+    const clickedTimeframeBtn = e.target.closest('.time-tracker__timeframe-btn');
+
+    if (!clickedTimeframeBtn || !e.target.dataset?.timeframe) return;
 
     timeframeListEl.querySelectorAll('.time-tracker__timeframe-btn').forEach(el => el.classList.remove('time-tracker__timeframe-btn--active'));
-    e.target.classList.add('time-tracker__timeframe-btn--active');
+    clickedTimeframeBtn.classList.add('time-tracker__timeframe-btn--active');
 
     state.selectedTimeframe = e.target.dataset.timeframe;
 
-    updateTimeTracker();
+    displayTimeTracker();
 });
 
 fetchData();
